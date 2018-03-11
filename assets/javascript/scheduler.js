859a277e7588c9274d8153e97ec50c39de216e01
Molly Cougill <molly.cougill@gmail.com>
2018-03-10 20:43:00 -0600

    edit styling, add ids

---
+++ assets/javascript/scheduler.js
@@ -0,0 +1,13 @@
+//firebase to host arrival and departure data
+//app to retrieve and manipulate this information with Moment.js
+//app will provide up-to-date information about various trains (arrival time, how many minutes remain until they arrive at their station)
+
+//append form entry to train table 
+//create variable to reference database
+var database = firebase.database();
+
+//store entry into firebase
+
+//firebase watcher + initial loader (.on("value"))
+//change HTML to reflect
+//Handle the errors 
