2b7343e3881a2f23f3fa8d9ecffc84ec1a394f52
Molly Cougill <molly.cougill@gmail.com>
2018-03-11 18:04:39 -0500

    push child objects to firebase

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -1,33 +1,56 @@
-//create variable to reference database
 
-var database = firebase.database();
+$(document).ready(function () {
 
-//append form entry to train table 
-$(".btn").on("click",function(event){
-    event.preventDefault();
+    var train = "";
+    var destination = "";
+    var firstTrain = "";
+    var freq = "";
+    var trainNum = [];
 
-    train = $("#trainNameInput").val().trim();
-      destination = $("#destinationInput").val().trim();
-      firstTrain = $("#firstTrainInput").val().trim();
-      freq = $("#frequencyInput").val().trim();
+    //create variable to reference database
+    var database = firebase.database();
 
-      //store entry into firebase
-      database.ref().set({
-        train: train,
-        destination: destination,
-        firstTrain: firstTrain,
-        freq: freq
+    //grab value from form input
+    $(".btn").on("click", function (event) {
+        event.preventDefault();
 
-});
-});
+        train = $("#trainNameInput").val().trim();
+        destination = $("#destinationInput").val().trim();
+        firstTrain = $("#firstTrainInput").val().trim();
+        freq = $("#frequencyInput").val().trim();
+
+        //store entry into firebase
+        database.ref().push({
+            train: train,
+            destination: destination,
+            firstTrain: firstTrain,
+            freq: freq
+
+        });
+
+        return false;
+    });
+    //append input every time child added from form
+    database.ref().on("child_added", function (snapshot) {
+        
+       
+       // $("#trainView").append("<td>"+train +"</td>");
+        
+        //$("#destinationDisplay").append(destination);
+        //$("#frequencyDisplay").append(freq);
+
+    })
 
 
 
-//firebase to host arrival and departure data
-//app to retrieve and manipulate this information with Moment.js
-//app will provide up-to-date information about various trains (arrival time, how many minutes remain until they arrive at their station)
 
-//firebase watcher + initial loader (.on("value"))
-//change HTML to reflect
-//Handle the errors 
 
+
+    //firebase to host arrival and departure data
+    //app to retrieve and manipulate this information with Moment.js
+    //app will provide up-to-date information about various trains (arrival time, how many minutes remain until they arrive at their station)
+
+    //firebase watcher + initial loader (.on("value"))
+    //change HTML to reflect
+    //Handle the errors 
+})
